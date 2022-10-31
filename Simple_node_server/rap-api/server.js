f0d489e22ab257d3ce8cb4268716a5077624a62e
NicolasEiriz <eiriznicolas@gmail.com>
2022-10-30 23:10:23 -0300

    Modify files

--- Simple_node_server/rap-api/server.js
+++ Simple_node_server/rap-api/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+
+app.use(cors())
+
+let savage = {
+  'age': 30,
+  'birthName': 'Shéyaa Bin Abraham-Joseph',
+  'birthLocation': 'London, England'
+}
 
 app.get('/', (request, response)=>{
   response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/savage', (request, response)=>{
+  response.json(savage)
 })
 
 app.listen(PORT, ()=>{
