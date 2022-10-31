789adc30250cd244d0971b3e7273618240207f16
NicolasEiriz <eiriznicolas@gmail.com>
2022-10-31 16:00:51 -0300

    modify files

--- Simple_node_server/rap-api/server.js
+++ Simple_node_server/rap-api/server.js
@@ -6,18 +6,32 @@ const PORT = 8000
 
 app.use(cors())
 
-let savage = {
-  'age': 30,
-  'birthName': 'Shéyaa Bin Abraham-Joseph',
-  'birthLocation': 'London, England'
-}
+let rappers = {
+  '21 savage':{
+    'age': 30,
+    'birthName': 'Shéyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England'
+  },
+  '50 cent':{
+    'age':  47,
+    'birthName': 'Curtis James Jackson III',
+    'birthLocation': 'Queens, NEw York City,US'
+  }
+
+} 
 
 app.get('/', (request, response)=>{
   response.sendFile(__dirname + '/index.html')
-})
+}) 
 
-app.get('/api/savage', (request, response)=>{
-  response.json(savage)
+app.get('/api/rappers/:rapperName', (request, response)=>{
+  const rapperName = request.params.rapperName.toLowerCase()
+  console.log(rapperName);
+  if(rappers[rapperName]){
+    response.json(rappers[rapperName])
+  } else{
+    response.json('rapper not uploaded yet')
+  }
 })
 
 app.listen(PORT, ()=>{
